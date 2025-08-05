bed3300d11197b7564af1efb566555bdaa924945
Christina Grannas <christina.grannas@gmail.com>
2025-08-05 10:10:14 +0200

    Test change

--- designsystem/bit-cloud/components/fact-box/fact-box.js
+++ designsystem/bit-cloud/components/fact-box/fact-box.js
@@ -1,11 +1,18 @@
 /** @jsx jsx */
-import React from 'react'
-import { jsx } from '@emotion/core'
-import {wrapperStyling, themeBlue, themeGrey, titleStyling, iconWrapper, iconWrapperReport, removeMargin, iconColor, articleEntryMarginStyling} from './fact-box.css.js'
+import React from 'react';
+import { jsx } from '@emotion/core';
 import {
-  Icon
-} from '@konsumentverket-sverige/designsystem.icon';
-
+  wrapperStyling,
+  themeBlue,
+  themeGrey,
+  titleStyling,
+  iconWrapper,
+  iconWrapperReport,
+  removeMargin,
+  iconColor,
+  articleEntryMarginStyling,
+} from './fact-box.css.js';
+import { Icon } from '@konsumentverket-sverige/designsystem.icon';
 
 export const FactBox = ({
   children,
@@ -17,41 +24,37 @@ export const FactBox = ({
   articleEntryMargin = false,
   reportIcon = false,
   noMargin = false,
-  themeColor = "blue"
+  themeColor = 'blue',
 }) => {
-
   if (!content && !children) {
     return null;
   }
 
+  const hej = 'hej';
   return (
-    <div css={[
-      wrapperStyling,
-      articleEntryMargin ? articleEntryMarginStyling : null,
-      noMargin ? removeMargin : null,
-      themeColor === "grey" ? themeGrey : themeBlue
-    ]}
-     data-comp="fact-box"
-     data-contentful-field-id={contentfulName ?? "considerationBox"}
-     data-contentful-entry-id={contentfulId}
+    <div
+      css={[
+        wrapperStyling,
+        articleEntryMargin ? articleEntryMarginStyling : null,
+        noMargin ? removeMargin : null,
+        themeColor === 'grey' ? themeGrey : themeBlue,
+      ]}
+      data-comp="fact-box"
+      data-contentful-field-id={contentfulName ?? 'considerationBox'}
+      data-contentful-entry-id={contentfulId}
     >
       {tag && (
-        <div css={[
-          iconWrapper,
-          reportIcon ? iconWrapperReport : null
-        ]}>
+        <div css={[iconWrapper, reportIcon ? iconWrapperReport : null]}>
           <Icon
-            icon={reportIcon ? "Information" : "MonoBookmark"}
+            icon={reportIcon ? 'Information' : 'MonoBookmark'}
             style={iconColor}
           />
         </div>
       )}
 
-      {headline && (
-        <h2 css={titleStyling}>{headline}</h2>
-      )}
+      {headline && <h2 css={titleStyling}>{headline}</h2>}
       {children}
       {content}
     </div>
-  )
-}
+  );
+};

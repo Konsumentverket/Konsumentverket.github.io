defe089ea34d7c9cb7af836081c9f97b8b5e61a1
Ida Strindberg <ida.strindberg@frojd.se>
2025-11-26 08:30:58 +0100

    Remove old components

--- designsystem/components/Headings/Heading.js
+++
@@ -1,6 +0,0 @@
-/** @jsx jsx */
-import { jsx } from '@emotion/core'
-import React from 'react';
-import { SubHeading } from '../SubHeading/SubHeading';
-
-export const Heading = ({text,style,children,...otherAttr}) => <SubHeading data-nosnippet="true" data-addsearch="exclude" style={style} level={1} text={text} {...otherAttr}>{children}</SubHeading>
--- designsystem/components/NotesLoader.js
+++
@@ -1,8 +0,0 @@
-﻿import find from 'lodash/find'
-
-export default function (name) {
-    let componentText = find(window.graphQLResponse.data.designsystemcomponentCollection.items, { componenter: name });
-    if (componentText === undefined) return '';
-    return "#" + componentText.heading + "\n  " + componentText.mainbody;
-
-}

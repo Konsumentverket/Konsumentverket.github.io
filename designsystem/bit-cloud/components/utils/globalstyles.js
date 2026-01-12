1dd45b2cc9d01126701717cbd7ef65129b6be86a
Jacob Hytter <jacob.hytter@gmail.com>
2026-01-12 18:15:24 +0100

    kovDarkModeBlue

--- designsystem/bit-cloud/components/utils/globalstyles.js
+++ designsystem/bit-cloud/components/utils/globalstyles.js
@@ -281,7 +281,7 @@ const globalStyles = (
         fill: ${newColors.shades.mediumBlue};
       }
       &:hover {
-        box-shadow: 0 0 0 4px ${newColors.primaries.kovBlue50};
+        box-shadow: 0 0 0 4px ${newColors.kovDarkModeBlue};
         background-color: ${newColors.primaries.fullBlue};
         color: ${newColors.shades.mediumBlue};
         svg {
@@ -296,7 +296,7 @@ const globalStyles = (
       }
 
       &:active {
-        box-shadow: 0 0 0 4px ${newColors.primaries.kovBlue50};
+        box-shadow: 0 0 0 4px ${newColors.kovDarkModeBlue};
         background-color: ${newColors.primaries.fullBlue};
         color: ${newColors.shades.mediumBlue};
         svg {

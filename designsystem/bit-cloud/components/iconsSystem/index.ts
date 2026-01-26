6b7583a4e4aee1e7b79a5953456a9a4787cb8363
Christina Grannas <christina.grannas@gmail.com>
2026-01-26 12:05:14 +0100

    Add Close icon

--- designsystem/bit-cloud/components/iconsSystem/index.ts
+++ designsystem/bit-cloud/components/iconsSystem/index.ts
@@ -21,6 +21,7 @@ export * from './SystemIcons/ChevronRight/ChevronRight.js';
 export * from './SystemIcons/CircledCheckmark/CircledCheckmark.js';
 export * from './SystemIcons/CircledX/CircledX.js';
 export * from './SystemIcons/Clear/Clear.js';
+export * from './SystemIcons/Close/Close.js';
 export * from './SystemIcons/DBDeleteFileAlt2/DBDeleteFileAlt2.js';
 export * from './SystemIcons/DBFileExcel/DBFileExcel.js';
 export * from './SystemIcons/DBFileImage/DBFileImage.js';

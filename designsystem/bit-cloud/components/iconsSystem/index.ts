bd0d78f8e0e32098cab829dca387acdbdfa2825b
Ida Strindberg <ida.strindberg@frojd.se>
2026-01-30 11:17:19 +0100

    Add icon

--- designsystem/bit-cloud/components/iconsSystem/index.ts
+++ designsystem/bit-cloud/components/iconsSystem/index.ts
@@ -100,3 +100,4 @@ export * from './SystemIcons/DefaultUser/DefaultUser.js';
 export * from './SystemIcons/Plus/Plus.js';
 export * from './SystemIcons/Trash/Trash.js';
 export * from './SystemIcons/Lock/Lock.js';
+export * from './SystemIcons/Save/Save.js';

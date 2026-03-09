9271c8ed9f84ed13f8646b9316409aacb79a2a22
Jacob Hytter <jacob.hytter@consultant.volvo.com>
2026-03-09 12:47:48 +0100

    PlusBlue

--- designsystem/bit-cloud/components/iconsSystem/index.ts
+++ designsystem/bit-cloud/components/iconsSystem/index.ts
@@ -100,6 +100,7 @@ export * from './SystemIcons/TextEditorUnorderedList/TextEditorUnorderedList.js'
 export * from './SystemIcons/TextEditorLink/TextEditorLink.js';
 export * from './SystemIcons/DefaultUser/DefaultUser.js';
 export * from './SystemIcons/Plus/Plus.js';
+export * from './SystemIcons/PlusBlue/PlusBlue.js';
 export * from './SystemIcons/Trash/Trash.js';
 export * from './SystemIcons/Lock/Lock.js';
 export * from './SystemIcons/Save/Save.js';
